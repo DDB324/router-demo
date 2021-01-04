99c55a0a9ec7f7321ecc022db7f3ffb3eaee8c6d
ddb324 <jiangdaoran@163.com>
2021-01-04 10:44:24 +0800

    404路由

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 let route = () => {
     const visibleWrapper = document.querySelector('#app')
     const number = window.location.hash.slice(1) || 1
-    const div = document.querySelector(`#div${number}`)
+    let div = document.querySelector(`#div${number}`)
     if (visibleWrapper.children.length > 0) {
         const removeDiv = visibleWrapper.children[0]
         removeDiv.style.display = 'none'
         document.body.appendChild(removeDiv)
+    }
+    if (!div) {
+        div = document.querySelector('#div404')
     }
     div.style.display = 'block'
     visibleWrapper.appendChild(div)
